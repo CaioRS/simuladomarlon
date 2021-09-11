1669ffcd9b56cc76f0946c6f0b9dedc541b8130c
Caio Roberto Souza Santos <caiorss@ucl.br>
2021-09-10 22:13:43 -0300

    final

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    url = "https://meme-api.herokuapp.com/gimme"
-    response = requests.get(url)
-    dados = response.json()
-    return render_template("index.html",info=dados)
+
+    lista= []
+
+    for x in range(0,6):
+        url = "https://meme-api.herokuapp.com/gimme"
+        response = requests.get(url)
+        dados = response.json()
+        lista.append(dados)
+
+    print (lista)
+    return render_template("index.html",info=lista)
+
 
 
 if __name__ == '__main__':
